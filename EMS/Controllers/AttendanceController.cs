34b6d8ef47ea5a5b1cfd16f17062750b48e06cba
Ulindus <ulindusathsara@gmail.com>
2026-01-03 03:19:20 +0530

    add atendence controller

---
+++ EMS/Controllers/AttendanceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Controllers
+{
+    public class AttendanceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

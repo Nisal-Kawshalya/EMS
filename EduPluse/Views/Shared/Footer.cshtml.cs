86da8e59658f28520ce4ff8c1755b6d3ff8183e9
Ulindus <ulindusathsara@gmail.com>
2026-01-03 02:14:21 +0530

    Create a footer

---
+++ EduPluse/Views/Shared/Footer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EMS.Views.Shared
+{
+    public class FooterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

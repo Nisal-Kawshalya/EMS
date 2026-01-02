a12ab519d2c394eb698f3d62baafd0e2247db4ee
Nisal-Kawshalya <ict22053@std.uwu.ac.lk>
2026-01-03 03:07:17 +0530

    Create Teacher and Class Controllers and their views

---
+++ EMS/Controllers/TeacherController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EMS.Data;
+
+public class TeacherController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public TeacherController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // TEACHER DASHBOARD
+    public IActionResult Dashboard()
+    {
+        // get logged-in user id
+        int userId = HttpContext.Session.GetInt32("UserId")!.Value;
+
+        // find teacher
+        var teacher = _context.Teachers
+            .FirstOrDefault(t => t.UserId == userId);
+
+        // get teacher classes
+        var classes = _context.Classes
+            .Where(c => c.TeacherId == teacher!.Id)
+            .Include(c => c.ClassStudents)
+            .ToList();
+
+        return View(classes);
+    }
+}

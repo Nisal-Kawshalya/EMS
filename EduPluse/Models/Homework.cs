a4b3b0866d3ad3b3cb3968fc3f7a9f7a4bbe0c32
Nisal-Kawshalya <ict22053@std.uwu.ac.lk>
2026-01-03 01:11:37 +0530

    Full System Update

--- EduPluse/Models/Homework.cs
+++ EduPluse/Models/Homework.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace EduPluse.Models
+namespace EMS.Models
 {
     public class Homework
     {

19878532ed69a64de1c17be4d13f74f3f07658b4
Ulindus <ulindusathsara@gmail.com>
2026-01-03 06:44:02 +0530

    studentcontroller

--- EMS/Controllers/StudentsController.cs
+++ EMS/Controllers/StudentsController.cs
@@ -3,69 +3,74 @@ using Microsoft.EntityFrameworkCore;
 using EMS.Data;
 using EMS.Models;
 
-public class StudentsController : Controller
+namespace EMS.Controllers
 {
-    private readonly ApplicationDbContext _context;
-
-    public StudentsController(ApplicationDbContext context)
+    public class StudentsController : Controller
     {
-        _context = context;
-    }
+        private readonly ApplicationDbContext _context;
 
-    // 👨‍🎓 SHOW STUDENTS TAB
-    public IActionResult Index(int classId)
-    {
-        var cls = _context.Classes
-            .Include(c => c.ClassStudents)
-                .ThenInclude(cs => cs.Student)
-            .FirstOrDefault(c => c.Id == classId);
-
-        if (cls == null)
-            return NotFound();
-
-        ViewBag.Class = cls;
-        return View(cls.ClassStudents.ToList());
-    }
-
-    // ➕ ADD STUDENT BY STUDENT CODE
-    [HttpPost]
-    public IActionResult Add(int classId, string studentCode)
-    {
-        var student = _context.Students
-            .FirstOrDefault(s => s.StudentCode == studentCode);
-
-        if (student == null)
+        public StudentsController(ApplicationDbContext context)
         {
-            TempData["Error"] = "Student not found";
+            _context = context;
+        }
+
+        // 📘 STUDENTS TAB
+        public IActionResult Index(int classId)
+        {
+            var cls = _context.Classes
+                .Include(c => c.ClassStudents)
+                    .ThenInclude(cs => cs.Student)
+                .FirstOrDefault(c => c.Id == classId);
+
+            if (cls == null)
+                return NotFound();
+
+            ViewBag.Class = cls;
+            return View(cls.ClassStudents.ToList());
+        }
+
+        // ➕ ADD STUDENT USING STUDENT CODE
+        [HttpPost]
+        public IActionResult Add(int classId, string studentCode)
+        {
+            var student = _context.Students
+                .FirstOrDefault(s => s.StudentCode == studentCode);
+
+            if (student == null)
+            {
+                TempData["Error"] = "Student not found";
+                return RedirectToAction("Index", new { classId });
+            }
+
+            bool alreadyAdded = _context.ClassStudents
+                .Any(cs => cs.ClassId == classId && cs.StudentId == student.Id);
+
+            if (!alreadyAdded)
+            {
+                _context.ClassStudents.Add(new ClassStudent
+                {
+                    ClassId = classId,
+                    StudentId = student.Id
+                });
+
+                _context.SaveChanges();
+            }
+
             return RedirectToAction("Index", new { classId });
         }
 
-        bool exists = _context.ClassStudents
-            .Any(cs => cs.ClassId == classId && cs.StudentId == student.Id);
-
-        if (!exists)
+        // 🗑 REMOVE STUDENT
+        public IActionResult Remove(int id, int classId)
         {
-            _context.ClassStudents.Add(new ClassStudent
+            var cs = _context.ClassStudents.Find(id);
+
+            if (cs != null)
             {
-                ClassId = classId,
-                StudentId = student.Id
-            });
-            _context.SaveChanges();
+                _context.ClassStudents.Remove(cs);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index", new { classId });
         }
-
-        return RedirectToAction("Index", new { classId });
-    }
-
-    // 🗑 REMOVE STUDENT FROM CLASS
-    public IActionResult Remove(int id, int classId)
-    {
-        var cs = _context.ClassStudents.Find(id);
-        if (cs != null)
-        {
-            _context.ClassStudents.Remove(cs);
-            _context.SaveChanges();
-        }
-
-        return RedirectToAction("Index", new { classId });
     }
 }
